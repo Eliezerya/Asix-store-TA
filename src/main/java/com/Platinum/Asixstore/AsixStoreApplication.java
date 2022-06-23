7a2526afac45ff66ea0f15a350ca0ee9f8ed0fa8
hendra <inibuatapasih@gmail.com>
2022-06-23 19:49:51 +0700

    try 2

--- src/main/java/com/Platinum/Asixstore/AsixStoreApplication.java
+++ src/main/java/com/Platinum/Asixstore/AsixStoreApplication.java
@@ -7,15 +7,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableWebMvc
 public class AsixStoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsixStoreApplication.class, args);
 	}
 	@Bean
+
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
